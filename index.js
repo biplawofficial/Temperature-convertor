885e9442aadb93b0735bc086769e4d8b05a6f35d
panther1407 <125789886+panther1407@users.noreply.github.com>
2024-03-26 00:43:45 +0530

    added the files into the repo
    first import

---
+++ index.js
@@ -0,0 +1,29 @@
+const value=document.getElementById("temp");
+const output=document.getElementById("henry");
+const celtofah=document.getElementById("celtofah");
+const fahtocel=document.getElementById("fahtocel");
+const button=document.getElementById("submit");
+const form=document.getElementById("formm");
+let temp;
+function convert()
+{
+   
+    if(fahtocel.checked)
+    {
+       temp=Number(value.value);
+       temp=((temp-32)*5/9).toFixed(1);
+       output.textContent=`${temp} °C`;
+    }
+    else if(celtofah.checked)
+    {
+        temp=Number(value.value);
+        temp=((temp*9/5)+32).toFixed(1);
+        output.textContent=`${temp} °F`;
+    }
+    else
+    {
+        output.textContent="Enter any value";
+    }
+    form.translate(20);
+    
+}
